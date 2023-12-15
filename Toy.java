d29199be77456f50c2c92df59d8a26ddac3501c5
Aleksandr Bolokhov <bolohov@inbox.ru>
2023-12-15 15:20:29 +0300

    homework

---
+++ Toy.java
@@ -0,0 +1,25 @@
+public class Toy {
+    private int id;
+    private String name;
+    private double frequency;
+
+    public Toy (int id, String name, double frequency) {
+        this.id = id;
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getFrequency() {
+        return frequency;
+    }
+    
+    
+}
